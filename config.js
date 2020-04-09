505b19a6f43bf6459614fd20d2444f6502fbe095
WillzMu <wilfredmulenga@yahoo.com>
2020-04-09 21:47:00 +0200

    setup env

---
+++ config.js
@@ -0,0 +1,5 @@
+import { API_KEY } from 'react-native-dotenv'
+
+export default {
+  API_KEY
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import config from '../../config'
 const Home = () => {
   return (
     <View>
-      <Text>Hello world</Text>
+      <Text>{`Hello world. Here is my ${config.API_KEY}`}</Text>
     </View>
   )
 }
